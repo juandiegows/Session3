ed5be0abc67d092101e0ede8029249aeef637ecf
Juan Diego Mejía Maestre <Mejiamaestrejuandiego@gmail.com>
2021-05-23 12:22:29 -0500

    crear form busqueda

---
+++ Session3/FormBusqueda.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Session3
+{
+    public partial class FormBusquedadVuelo : Form
+    {
+        public FormBusquedadVuelo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("¿Desea Salir?", "Alerta", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
